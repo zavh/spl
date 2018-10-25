85bd8e35073296250fd82ee7166e826b97742e9a
Sabit Safwat Rahman <rahman.sabit@yahoo.com>
2018-10-25 11:31:36 +0600

    Implemented delete a bit differently

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -4,7 +4,7 @@
     <div class="box">
         <<ul>
             <li>
-                <h3>This is Project: </h3> <p>{{$assignment->task_name}}</p>
+                <h3>This is Task: </h3> <p>{{$assignment->task_name}}</p>
                 <h3> described as: </h3><p>{{$assignment->task_description}}</p>
                 <h3> given to: </h3><p>{{$assignment->user_id}}</p>
                 <h3> given on: </h3> <p>{{$assignment->task_date_assigned}}</p>
@@ -13,11 +13,21 @@
         </ul>
     </div>
     <a href="/tasks/{{$assignment->id}}/edit" class="btn btn-primary">Edit</a>
+    
     <a href="/tasks" class="btn btn-primary">Go Back</a>
 
     {{-- {!!Form::open(['action'=>['TasksController@destroy',$assignment->id],'method'=>'POST','class'=>'pull-right'])!!}
         {{Form::hidden('_method','DELETE')}}
         {{Form::submit('Delete',['class'=>'btn btn-danger'])}}
     {!!Form::close()!!} --}}
-    
+
+    <form method="POST" action="{{ route('tasks.destroy', [$assignment->id]) }}">
+        {{ csrf_field() }}
+        {{ method_field('DELETE') }}
+
+        <div class="form-group">
+            <input type="submit" class="btn btn-danger delete-user" value="Delete">
+        </div>
+    </form>
+       
 @endsection
