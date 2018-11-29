b5f8a23881f8945ec35f496d0218a41b866e32b8
Zaved Hossain <hossain.zaved@gmail.com>
2018-11-29 22:01:22 +0600

    Move

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,40 +1,12 @@
 <div class="my-1 bg-light rounded shadow-sm border" >
-@foreach ($tasks as $task)
-    <div class="media text-muted">
-        <div class="media-body m-0 small lh-125 border-bottom border-gray">
-            <div class="d-flex px-1 py-1 justify-content-between align-items-center w-100 border-bottom">
-                <span class="text-gray-dark">{{$task->task_name}}</span>
-                <span>
-                    <a 
-                        href="javascript:void(0)" 
-                        onclick="ajaxFunction('editTasks', '{{ $task->id }}' , 'taskdiv')"
-                        class='badge bg-light shadow-sm border'
-                        >
-                        &#9997;
-                    </a>
-                        &nbsp;&nbsp;
-                        <a href="javascript:void(0)" 
-                        onclick="deleteTask('{{$task->task_name}}','{{$task->id}}')"
-                        class='badge bg-light shadow-sm border'
-                        >
-                        &#x2718;
-                    </a>
-                </span>
-            </div>
-            <span class="text-danger">Deadline: {{$task->task_deadline}}</span>  
-            <span class="text-primary"> Weight: {{$task->weight}}</span>
-        </div>
-    </div>
-@endforeach
-
 <div class="accordion" id="taskAccordion">
 @foreach ($tasks as $index=>$task)
-    <div class="card">
-        <div id="heading{{$index}}" class='border-bottom d-flex px-1 py-1 justify-content-between align-items-center w-100'>
+    <div class="card small">
+        <div id="heading{{$index}}" class='border-bottom d-flex justify-content-between align-items-center w-100'>
             <button class="btn btn-link btn-sm" type="button" data-toggle="collapse" data-target="#collapse{{$index}}" aria-expanded="true" aria-controls="collapseOne">
-            {{$task->task_name}}
+            {{$task->task_name}} <span class='badge badge-warning border border-dark'>{{$task->weight}}</span>
             </button>
-            <span>
+            <span class='mx-1'>
                     <a 
                         href="javascript:void(0)" 
                         onclick="ajaxFunction('editTasks', '{{ $task->id }}' , 'taskdiv')"
@@ -53,8 +25,10 @@
         </div>
 
         <div id="collapse{{$index}}" class="collapse  @if($index == 0) show @endif" aria-labelledby="heading{{$index}}" data-parent="#taskAccordion">
-        <div class="card-body">
-            {{$task->task_name}}
+        <div class="bg-light">
+            <div class='border-bottom'> <p class='px-4'>{{$task->task_description}}</p></div>
+            <div class="text-danger border-bottom"> <span class='px-4'> Deadline: {{$task->task_deadline}}</span></div>
+            <div class="text-primary"> <span class='px-4'>Weight: {{$task->weight}}</span></div>
         </div>
         </div>
     </div>
